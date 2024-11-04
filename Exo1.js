1c1bd3afc17ca2f0f1f5dc079b075a2e5730fe4a
DialloMoahamed <mohamedattahirdiallo@gmail.com>
2024-11-04 15:43:38 +0100

    Question 3 terminer

--- Exo1.js
+++ Exo1.js
@@ -6,3 +6,8 @@ console.table(countries)
 let transformation = {countries}
 console.table(transformation);
 
+
+// Reponse numero 3
+console.group('countries')
+console.log(countries);
+console.groupEnd()
